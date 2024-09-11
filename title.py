b46798c4c1187ac777f8011c229228419a98ef82
Anuj  Singh <t-anuj.singh@foetron.com>
2024-09-11 15:59:28 +0530

    anuj

--- title.py
+++ title.py
@@ -1,3 +1,107 @@
-a="abc ANCNA  adas"
+# # # # # ##Strings
+# # # # # print ("Hello")
+# # # # # a= "hello"
+# # # # # print(a)
 
-print(a.title())
+# # # # # a= """"Lorem ipsum dolor sit amet,
+# # # # # consectetur adipiscing elit,
+# # # # # sed do eiusmod tempor incididunt
+# # # # # ut labore et dolore magna aliqua
+# # # # # """
+# # # # # print(a)
+
+
+# # # # # a = "Hello World!"
+# # # # # print(a[1])
+
+
+# # # # for i in "aaaaa":
+# # # #     print(i, end="")
+
+# # # # a = "Hello World"
+
+# # # # print(len(a))
+
+# # # a = "i am from up"
+# # # print("up" in a)
+
+
+# # a = " i hrjksdfhkdlg  up"
+# # if "i" in a:
+# #     print("Yes,'i' is present")
+
+
+# # a = ' ii alfnksdnlkfgnjkdfmnm'
+# # print("u" not in a)
+
+
+
+# # b= "hello"
+# # print(b[1:5])
+
+# # b = "anuj"
+# # print(b[:3])
+
+# # a = "anuj"
+# # print(b[2:])
+
+
+# # a = "anujsingh"
+# # print(b[-2:-5])
+
+# # a= "Hello,World!"
+# # print(a.upper())
+
+# # a = "JDFBSJDKFNBJKDF.NMD"
+# # print(a.lower())
+
+
+# # a ="                          hello                   "
+# # print(a.strip())
+
+
+# # a = "hnuj"
+# # print(a.replace("h" ,"A"))
+
+
+# # a = "anuj singh"
+# # print(a.split(","))
+
+
+# # a = "anuj"
+# # b= " singh"
+# # c = a+b
+# # print(c)
+
+
+# # a = "anuj"
+# # b ="anunffjds"
+# # c = a+" "+b
+# # print(c)
+
+
+# age =22
+# name = f"my name is anuj, i am {age}"
+# print(name)
+
+
+# a = 36
+# txt =f"I have {a} rs."
+# print(txt)
+
+price = 200
+txt = f"The price is {price:.2f} dollars"
+
+print (txt)
+
+
+a =f"i have {2*2} rs."
+
+print(a)
+
+
+b = "Heyy \"anuj\" from the north"
+print(b)
+
+a= "anujsingh"
+print(a[2:6])
